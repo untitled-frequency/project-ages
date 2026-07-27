16cc61b9af1bbe864226f963d3de021ebeca627c
untitled-frequency <untitled.njr@gmail.com>
2026-07-27 13:00:24 +0100

    user factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,18 +10,34 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['nom', 'email', 'password', 'tel']) ]
 #[Hidden(['password', 'remember_token'])]
+
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'nom',
+        'email',
+        'password',
+        'tel',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
+
+    public $timestamps = false;
+    
     protected function casts(): array
     {
         return [
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 export default function Dashboard() {
     return (
@@ -16,11 +16,12 @@ export default function Dashboard() {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in!
+
                         </div>
                     </div>
                 </div>
             </div>
         </AuthenticatedLayout>
+
     );
 }
